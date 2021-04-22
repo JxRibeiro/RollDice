39ad574f77314e2ef1d650eb47c9aa20494184bd
Joao <joaomarcio.ribeiro@gmail.com>
2021-04-22 10:00:06 -0300

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -4,12 +4,16 @@ import random
 
 def rollDice():
 	keepRolling = ''
+	
 	while keepRolling != 'n':
+		diceSum = 0
 		i = int(input('How many dices you want to roll? '))
 		for j in range(i):
 			
 			dice = random.randint(1,6)
+			diceSum = diceSum + dice
 			print(f'Dice {j+1}: {dice} ')
+		print(f'Dice sum: {diceSum}')
 		keepRolling = input('Do you want to roll again? (y/n)').lower()
 	
 rollDice()
